4ee8d922e8f34a64243bbb9adad34fb9a7c73364
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2013-11-16 20:49:49 +0100

    Tabbing added

--- src/de/topobyte/jterm/JTerm.java
+++ src/de/topobyte/jterm/JTerm.java
@@ -1,9 +1,18 @@
 package de.topobyte.jterm;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
 
 import de.topobyte.jterm.core.TerminalMouseAdapter;
 import de.topobyte.jterm.core.TerminalWidget;
@@ -26,8 +35,14 @@ public class JTerm
 		Toolbar toolbar = new Toolbar();
 		content.add(toolbar, BorderLayout.NORTH);
 
+		final JTabbedPane tabbed = new JTabbedPane();
+		content.add(tabbed, BorderLayout.CENTER);
+		tabbed.setFocusable(false);
+
+		final String title = "term";
+
 		TerminalWidget terminalWidget = new TerminalWidget();
-		content.add(terminalWidget, BorderLayout.CENTER);
+		tabbed.add(title, terminalWidget);
 
 		Statusbar statusbar = new Statusbar();
 		content.add(statusbar, BorderLayout.SOUTH);
@@ -39,6 +54,26 @@ public class JTerm
 		frame.setLocationByPlatform(true);
 		frame.setSize(600, 500);
 		frame.setVisible(true);
-	}
 
+		String keyCtrlShiftT = "ctrl-shift-t";
+
+		InputMap inputMap = tabbed
+				.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		inputMap.put(
+				KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK
+						| InputEvent.SHIFT_DOWN_MASK), keyCtrlShiftT);
+		ActionMap actionMap = tabbed.getActionMap();
+		actionMap.put(keyCtrlShiftT, new AbstractAction() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				TerminalWidget terminalWidget = new TerminalWidget();
+				tabbed.add(title, terminalWidget);
+			}
+		});
+
+	}
 }
